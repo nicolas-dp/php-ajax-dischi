fdffa11f2b8e7ca7af32767e43641d895859ffd1
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-24 14:53:04 +0200

    MILESTONE 2- Completa

--- index.php
+++ index.php
@@ -2,28 +2,14 @@
 Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
 
 <?php
-include __DIR__ . '/db.php'
+include __DIR__ . '/db.php';
 
-?>
-
-<!doctype html>
-<html lang="en">
-
-<head>
-    <title>PHP Dischi</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-</head>
-
-<body>
+include __DIR__ . '/layout/head.php'; ?>
+<main>
     <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 g-4">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-6 g-4 justify-content-center">
             <?php foreach ($dischi as $disco) : ?>
-                <div class="col bg-dark text-white">
+                <div class="col bg-dark text-white p-4 m-1">
                     <img src="<?= $disco['poster'] ?>" class="img-fluid" alt="<?= $disco['title'] ?>">
                     <div class="card-body">
                         <h5 class="card-title"><?= $disco['title'] ?></h5>
@@ -39,7 +25,6 @@ include __DIR__ . '/db.php'
             <?php endforeach ?>
         </div>
     </div>
+</main>
 
-</body>
-
-</html>
+<?php include __DIR__ . '/layout/footer.php'; ?>
---
+++ layout/head.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>PHP Dischi</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="./img/spotify.png" alt="logo" height="50" width="50" />
+                </a>
+            </div>
+        </nav>
+    </header>
