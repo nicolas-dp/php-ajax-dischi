50f72357c86dfea82aee2d1418d6caadb97a7f51
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-24 14:54:28 +0200

    MILESTONE 1- Completa - rettifica per err. commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- Prima Milestone:
+<!-- MILESTONE 1:
 Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
 
 <?php
